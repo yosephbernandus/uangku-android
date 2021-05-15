cf82bf11918ba2ee4356622380481a4087c08741
yosephbernandus <yosephbernandus@gmail.com>
2021-05-15 12:14:20 +0700

    Add register page (design)

--- app/src/main/java/mobile/uangku/android/activities/auth/RegisterActivity.kt
+++ app/src/main/java/mobile/uangku/android/activities/auth/RegisterActivity.kt
@@ -8,18 +8,18 @@ import mobile.uangku.android.R
 import mobile.uangku.android.activities.TabActivity
 import mobile.uangku.android.core.Session
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
         if (Session.isAuthenticated(this)){
             startActivity(Intent(this, TabActivity::class.java))
         }
     }
 
-    fun signUpOnClick(view: View) {
-        startActivity(Intent(this, RegisterActivity::class.java))
+    fun signInOnClick(view: View) {
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
