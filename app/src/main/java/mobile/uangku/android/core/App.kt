cedef710ce9e3ccf25af1e9cb28990beb590cc07
Yoseph Bernandus <yosephbernandus@gmail.com>
2021-05-30 01:22:43 +0700

    add realm initialization (#4)

--- app/src/main/java/mobile/uangku/android/core/App.kt
+++ app/src/main/java/mobile/uangku/android/core/App.kt
@@ -1,4 +1,28 @@
 package mobile.uangku.android.core
 
-class App {
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import io.realm.exceptions.RealmMigrationNeededException
+import mobile.uangku.android.models.Migration
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+                .schemaVersion(0)
+                .migration(Migration())
+                .build()
+        try {
+            Realm.getInstance(realmConfig)
+        } catch (e: RealmMigrationNeededException) {
+            e.printStackTrace()
+            Realm.deleteRealm(realmConfig)
+            Preferences(this).clear()
+        }
+        Realm.setDefaultConfiguration(realmConfig)
+    }
 }
