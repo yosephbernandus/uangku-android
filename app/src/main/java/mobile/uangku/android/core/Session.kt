36528d40b61f7f17d62aa03c2402fac513372016
Yoseph Bernandus <yosephbernandus@gmail.com>
2021-05-30 01:14:51 +0700

    Add API Connect (#2)

--- app/src/main/java/mobile/uangku/android/core/Session.kt
+++ app/src/main/java/mobile/uangku/android/core/Session.kt
@@ -2,6 +2,9 @@ package mobile.uangku.android.core
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import io.realm.Realm
+import mobile.uangku.android.activities.auth.LoginActivity
 import org.json.JSONObject
 
 object Session {
@@ -17,5 +20,21 @@ object Session {
     fun userKeySet(activity: Activity, response: JSONObject) {
         val preferences = Preferences(activity)
         preferences.set("sessionKey", response.getString("session_key"))
+    }
+
+    fun clear(context: Context) {
+        val preferences = Preferences(context)
+        preferences.clear()
+
+        val realm = Realm.getDefaultInstance()
+        realm.beginTransaction()
+        realm.deleteAll()
+        realm.commitTransaction()
+
+        val intent = Intent(context, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        intent.putExtra("action", "logout")
+        context.startActivity(intent)
+        (context as Activity).finish()
     }
 }
