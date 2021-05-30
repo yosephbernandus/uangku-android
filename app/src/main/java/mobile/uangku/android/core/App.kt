fd8acfae3811d0771f8b9b5e731a0f34847e543c
yosephbernandus <yosephbernandus@gmail.com>
2021-05-30 13:55:53 +0700

    add password show hide button

--- app/src/main/java/mobile/uangku/android/core/App.kt
+++ app/src/main/java/mobile/uangku/android/core/App.kt
@@ -1,6 +1,7 @@
 package mobile.uangku.android.core
 
 import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import io.realm.exceptions.RealmMigrationNeededException
@@ -11,6 +12,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        Fresco.initialize(this)
         Realm.init(this)
         val realmConfig = RealmConfiguration.Builder()
                 .schemaVersion(0)
