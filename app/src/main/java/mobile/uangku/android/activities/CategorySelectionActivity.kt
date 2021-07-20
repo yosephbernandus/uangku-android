82a6e35c1911bad68973c154021f923c3e601e82
yosephbernandus <yosephbernandus@gmail.com>
2021-07-20 19:30:05 +0700

    add category item

---
+++ app/src/main/java/mobile/uangku/android/activities/CategorySelectionActivity.kt
@@ -0,0 +1,14 @@
+package mobile.uangku.android.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import mobile.uangku.android.R
+
+class CategorySelectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_selection)
+    }
+
+}
