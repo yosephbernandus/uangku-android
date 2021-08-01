cc38a69695ffdc196c5ac2a241852b75b6f9c8c4
yosephbernandus <yosephbernandus@gmail.com>
2021-08-01 07:48:57 +0700

    add api save goal

--- app/src/main/java/mobile/uangku/android/activities/goal/EditGoalActivity.kt
+++ app/src/main/java/mobile/uangku/android/activities/goal/EditGoalActivity.kt
@@ -1,14 +1,24 @@
 package mobile.uangku.android.activities.goal
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
 import com.google.android.material.datepicker.MaterialDatePicker
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_add_goal.*
 import mobile.uangku.android.R
 import mobile.uangku.android.activities.CategorySelectionActivity
-import mobile.uangku.android.core.DateUtils
+import mobile.uangku.android.core.*
+import mobile.uangku.android.models.Category
+import mobile.uangku.android.models.Goal
+import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.Locale
@@ -16,7 +26,9 @@ import java.util.Locale
 
 class EditGoalActivity : AppCompatActivity() {
 
+    lateinit var preferences: Preferences
     var goalDate: Calendar = Calendar.getInstance()
+    var selectedCategoryID: Int = 0
     val CATEGORY_REQUEST_CODE = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,4 +55,91 @@ class EditGoalActivity : AppCompatActivity() {
         }
     }
 
+    fun syncCategories() {
+        val loadingDialog = LoadingDialog(this)
+
+        loadingDialog.show()
+
+        val request = API.createGetRequest(this, "categories/", null)
+        request.getAsJSONObject(object : JSONObjectRequestListener {
+            override fun onResponse(response: JSONObject) {
+                loadingDialog.dismissIfNeeded()
+
+                val realm = Realm.getDefaultInstance()
+                realm.executeTransactionAsync { bgRealm ->
+                    Category.fromJSONArray(bgRealm, response.getJSONArray("categories"))
+                }
+            }
+
+            override fun onError(error: ANError) {
+                loadingDialog.dismissIfNeeded()
+                API.handleErrorResponse(this@EditGoalActivity, error)
+            }
+        })
+    }
+
+    fun closeIconOnClick(view: View) {
+        finish()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val loadingDialog = LoadingDialog(this)
+        if (loadingDialog != null && loadingDialog.isShowing) {
+            loadingDialog.dismiss()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CATEGORY_REQUEST_CODE && resultCode == RESULT_OK) {
+            selectedCategoryID = data!!.getIntExtra("categoryID", 0)
+            goalCategory.setText(data!!.getStringExtra("name"))
+        }
+    }
+
+    fun submitButtonOnClick(view: View) {
+        if (!Validator.validateMinLength(amountTextView, 1)) {
+            Alert.dialog(this, "Mohon isi jumlah uang terlebih dahulu")
+        } else if(achievmentDate.text.isNullOrEmpty()) {
+            Alert.dialog(this, "Mohon isi tanggal terlebih dahulu")
+        } else if(selectedCategoryID == 0) {
+            Alert.dialog(this, "Mohon pilih category terlebih dahulu")
+        } else {
+            val loadingDialog = LoadingDialog(this)
+            loadingDialog.show()
+
+            // TODO: Add name and deposit cycle
+            val androidNetworking = AndroidNetworking.upload(API.getBaseURL(this) + "goals/edit-saving-goals")
+                .addMultipartParameter("token", API.TOKEN)
+                .addMultipartParameter("session_key", preferences.getString("sessionKey"))
+                .addMultipartParameter("category", selectedCategoryID.toString())
+                .addMultipartParameter("amount", amountTextView.text.toString())
+                .addMultipartParameter("achievement_date", achievmentDate.text.toString())
+                .addMultipartParameter("name", "Rumah")
+                .addMultipartParameter("deposit_cycle", "2")
+
+
+            val activity = this
+            androidNetworking.setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONObject(object : JSONObjectRequestListener {
+                    override fun onResponse(response: JSONObject) {
+                        loadingDialog.dismissIfNeeded()
+                        val realm = Realm.getDefaultInstance()
+                        realm.executeTransactionAsync(Realm.Transaction{ bgRealm ->
+                            Goal.fromJSON(bgRealm, response)
+                        }, Realm.Transaction.OnSuccess {
+                            setResult(Activity.RESULT_OK)
+                            finish()
+                        })
+                    }
+
+                    override fun onError(error: ANError) {
+                        loadingDialog.dismissIfNeeded()
+                        API.handleErrorResponse(this@EditGoalActivity, error)
+                    }
+                })
+        }
+    }
 }
