6f13d95e6a2b6944894eda4249bb90b6164fc327
Yosephbernandus <yosephbernandus@gmail.com>
2021-09-11 15:07:20 +0700

    add sync transactions

--- app/src/main/java/mobile/uangku/android/activities/transaction/TransactionFragment.kt
+++ app/src/main/java/mobile/uangku/android/activities/transaction/TransactionFragment.kt
@@ -6,14 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import io.realm.Realm
 import kotlinx.android.synthetic.main.fragment_goal.*
 import mobile.uangku.android.R
+import mobile.uangku.android.core.API
+import mobile.uangku.android.core.LoadingDialog
 import mobile.uangku.android.core.Preferences
+import mobile.uangku.android.core.Sync
+import mobile.uangku.android.models.Category
+import mobile.uangku.android.models.Transaction
+import org.json.JSONObject
 
 class TransactionFragment : Fragment() {
 
     lateinit var preferences: Preferences
     lateinit var fragmentContext: Context
+
+    val key ="transactions"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_transaction, container, false)
@@ -26,5 +37,63 @@ class TransactionFragment : Fragment() {
         swipeRefreshLayout.setOnRefreshListener {
             swipeRefreshLayout.isRefreshing = true
         }
+        syncCategory()
+    }
+
+    fun syncCategory() {
+        val loadingDialog = LoadingDialog(fragmentContext)
+        loadingDialog.show()
+
+        val request = API.createGetRequest(fragmentContext, "categories", null)
+        request.getAsJSONObject(object : JSONObjectRequestListener {
+            override fun onResponse(response: JSONObject) {
+                loadingDialog.dismissIfNeeded()
+                val realm = Realm.getDefaultInstance()
+                realm.executeTransactionAsync{ bgRealm ->
+                    Category.fromJSONArray(bgRealm, response.getJSONArray("categories"))
+                }
+            }
+
+            override fun onError(error: ANError) {
+                loadingDialog.dismissIfNeeded()
+                API.handleErrorResponse(fragmentContext, error)
+            }
+        })
+    }
+
+    fun syncTransaction(withLoadingDialog: Boolean = false) {
+        var loadingDialog: LoadingDialog? = null
+        if (withLoadingDialog) {
+            loadingDialog = LoadingDialog(fragmentContext)
+            loadingDialog.show()
+        }
+
+        val request = API.createGetRequest(fragmentContext, "transactions", null)
+        request.getAsJSONObject(object : JSONObjectRequestListener {
+            override fun onResponse(response: JSONObject) {
+                if (withLoadingDialog) loadingDialog!!.dismissIfNeeded()
+                if (!isVisible) return
+
+                if (swipeRefreshLayout != null) swipeRefreshLayout.isRefreshing = false
+                val realm = Realm.getDefaultInstance() as Realm
+
+                realm.executeTransactionAsync(Realm.Transaction { bgRealm ->
+                    Transaction.fromJSONArray(bgRealm, response.getJSONArray("transactions"))
+                }, Realm.Transaction.OnSuccess {
+                    Sync.setSyncTime(fragmentContext, key)
+                    setupUI()
+                })
+            }
+
+            override fun onError(error: ANError) {
+                if (withLoadingDialog) loadingDialog!!.dismissIfNeeded()
+                swipeRefreshLayout.isRefreshing = false
+                API.handleErrorResponse(fragmentContext, error)
+            }
+        })
+    }
+
+    fun setupUI(){
+
     }
 }
