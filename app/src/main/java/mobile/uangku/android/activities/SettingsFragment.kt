36fc2f2f4fee1c33872408dcd4907a208869fd0f
Yosephbernandus <yosephbernandus@gmail.com>
2021-09-18 11:41:36 +0700

    add settings fragment

--- app/src/main/java/mobile/uangku/android/activities/SettingsFragment.kt
+++ app/src/main/java/mobile/uangku/android/activities/SettingsFragment.kt
@@ -1,6 +1,25 @@
 package mobile.uangku.android.activities
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import mobile.uangku.android.R
+import mobile.uangku.android.core.Preferences
 
 class SettingsFragment : Fragment() {
+
+    lateinit var preferences: Preferences
+    lateinit var fragmentContext: Context
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 }
