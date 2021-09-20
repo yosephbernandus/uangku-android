6e131f23613bd2ae532767f7a1e2653ff090cf5c
Yosephbernandus <yosephbernandus@gmail.com>
2021-09-20 22:50:20 +0700

    home fragment last transaction

--- app/src/main/java/mobile/uangku/android/activities/HomeFragment.kt
+++ app/src/main/java/mobile/uangku/android/activities/HomeFragment.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.facebook.drawee.view.SimpleDraweeView
 import io.realm.Realm
 import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_tab.*
@@ -13,9 +17,11 @@ import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.photo
 import kotlinx.android.synthetic.main.fragment_home.placeholderProfilePhoto
 import kotlinx.android.synthetic.main.fragment_settings.*
+import kotlinx.android.synthetic.main.last_transaction_item.view.*
 import mobile.uangku.android.R
 import mobile.uangku.android.core.Preferences
 import mobile.uangku.android.core.Utils
+import mobile.uangku.android.models.Category
 import mobile.uangku.android.models.Transaction
 import mobile.uangku.android.models.UserData
 import java.util.*
@@ -56,6 +62,9 @@ class HomeFragment : Fragment() {
     }
 
     fun setupUI() {
+        if(!isAdded)
+            return
+
         val photoUrl = UserData.getUserPhotoUrl(fragmentContext)
 
         if (photoUrl != null) {
@@ -71,5 +80,49 @@ class HomeFragment : Fragment() {
             .findAll().sum("amount")
         incomeMonthAmount.text = "Rp. ${Utils.addThousandSeparator(incomeAmount.toDouble())}"
 
+        listTransactionRecylerView.visibility = View.VISIBLE
+        listTransactionRecylerView.adapter = RecyclerViewAdapter(transactions)
+        listTransactionRecylerView.layoutManager = LinearLayoutManager(fragmentContext, LinearLayoutManager.HORIZONTAL, false)
+        listTransactionRecylerView.isNestedScrollingEnabled = false
+    }
+
+    internal inner class RecyclerViewAdapter(var transactions: RealmResults<Transaction>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.last_transaction_item, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val transaction = transactions[position]
+            var amount = transaction!!.amount
+            var type = transaction!!.type
+            holder.id = transaction!!.id!!
+
+            if (transaction != null && transaction.categoryId != null) {
+                var category = Category[transaction.categoryId!!]
+                if (category != null)
+                    holder.categoryNameTextView.text = category.name
+                    holder.categoryIcon.setImageURI(category!!.logoUrl)
+            }
+            if (type == Transaction.Type.INCOME.ordinal) {
+                holder.transactionAmountTextView.text = "+ Rp. ${Utils.addThousandSeparator(amount)}"
+                holder.transactionAmountTextView.setTextColor(resources.getColor(R.color.linkSection))
+            } else {
+                holder.transactionAmountTextView.text = "- Rp. ${Utils.addThousandSeparator(amount)}"
+                holder.transactionAmountTextView.setTextColor(resources.getColor(R.color.minus))
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return transactions.size
+        }
+
+        internal inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            var id = 0
+            val transactionAmountTextView: TextView = view.transactionAmountTextView
+            val categoryNameTextView: TextView = view.categoryNameTextView
+            val categoryIcon: SimpleDraweeView = view.categoryIcon
+
+        }
     }
 }
